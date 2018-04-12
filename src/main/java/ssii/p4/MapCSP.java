99a2e2aabecd25f55c5dc7328a40860235b2cdac
Jorge <jorge.garcia@ext.docout.es>
2018-04-12 15:00:00 +0200

    Implementados cambios solicitados

--- src/main/java/ssii/p4/MapCSP.java
+++ src/main/java/ssii/p4/MapCSP.java
@@ -9,62 +9,70 @@ import aima.core.search.csp.Variable;
 /**
  * Artificial Intelligence A Modern Approach (3rd Ed.): Figure 6.1, Page 204.<br>
  * <br>
- * The principal states and territories of Australia. Coloring this map can be
- * viewed as a constraint satisfaction problem (CSP). The goal is to assign
- * colors to each region so that no neighboring regions have the same color.
- * 
+ * The principal states and territories of Australia. Coloring this map can be viewed as a
+ * constraint satisfaction problem (CSP). The goal is to assign colors to each region so that no
+ * neighboring regions have the same color.
+ *
  * @author Ruediger Lunde
  * @author Mike Stampone
  */
+@SuppressWarnings("javadoc")
 public class MapCSP extends CSP {
-	public static final Variable NSW = new Variable("NSW");
-	public static final Variable NT = new Variable("NT");
-	public static final Variable Q = new Variable("Q");
-	public static final Variable SA = new Variable("SA");
-	public static final Variable T = new Variable("T");
-	public static final Variable V = new Variable("V");
-	public static final Variable WA = new Variable("WA");
-	public static final String RED = "RED";
-	public static final String GREEN = "GREEN";
-	public static final String BLUE = "BLUE";
+  public static final Variable NSW = new Variable("NSW"); //$NON-NLS-1$
+  public static final Variable NT = new Variable("NT"); //$NON-NLS-1$
+  public static final Variable Q = new Variable("Q"); //$NON-NLS-1$
+  public static final Variable SA = new Variable("SA"); //$NON-NLS-1$
+  public static final Variable T = new Variable("T"); //$NON-NLS-1$
+  public static final Variable V = new Variable("V"); //$NON-NLS-1$
+  public static final Variable WV = new Variable("WV"); //$NON-NLS-1$
+  public static final Variable WA = new Variable("WA"); //$NON-NLS-1$
+  /** Nuevo color naranjito añadido */
+  public static final String NARANJITO = "ORANGE"; //$NON-NLS-1$
+  public static final String RED = "RED"; //$NON-NLS-1$
+  public static final String GREEN = "GREEN"; //$NON-NLS-1$
+  public static final String BLUE = "BLUE"; //$NON-NLS-1$
 
-	/**
-	 * Returns the principle states and territories of Australia as a list of
-	 * variables.
-	 * 
-	 * @return the principle states and territories of Australia as a list of
-	 *         variables.
-	 */
-	 private void collectVariables() {
-		addVariable(NSW);
-		addVariable(WA);
-		addVariable(NT);
-		addVariable(Q);
-		addVariable(SA);
-		addVariable(V);
-		addVariable(T);
-	}
+  /**
+   * Returns the principle states and territories of Australia as a list of variables.
+   *
+   * @return the principle states and territories of Australia as a list of variables.
+   */
+  private void collectVariables() {
+    addVariable(NSW);
+    addVariable(WA);
+    addVariable(NT);
+    addVariable(Q);
+    addVariable(SA);
+    addVariable(V);
+    addVariable(T);
+    // Añadida nueva región
+    addVariable(WV);
+  }
 
-	/**
-	 * Constructs a map CSP for the principal states and territories of
-	 * Australia, with the colors Red, Green, and Blue.
-	 */
-	public MapCSP() {
-		collectVariables();
+  /**
+   * Constructs a map CSP for the principal states and territories of Australia, with the colors
+   * Red, Green, and Blue.
+   */
+  public MapCSP() {
+    collectVariables();
 
-		Domain colors = new Domain(new Object[] { RED, GREEN, BLUE });
+    // Añadido nuevo color al dominio.
+    final Domain colors = new Domain(new Object[] {RED, GREEN, BLUE, NARANJITO});
 
-		for (Variable var : getVariables())
-			setDomain(var, colors);
+    for (final Variable var : getVariables()) {
+      setDomain(var, colors);
+    }
 
-		addConstraint(new NotEqualConstraint(WA, NT));
-		addConstraint(new NotEqualConstraint(WA, SA));
-		addConstraint(new NotEqualConstraint(NT, SA));
-		addConstraint(new NotEqualConstraint(NT, Q));
-		addConstraint(new NotEqualConstraint(SA, Q));
-		addConstraint(new NotEqualConstraint(SA, NSW));
-		addConstraint(new NotEqualConstraint(SA, V));
-		addConstraint(new NotEqualConstraint(Q, NSW));
-		addConstraint(new NotEqualConstraint(NSW, V));
-	}
+    addConstraint(new NotEqualConstraint(WA, NT));
+    addConstraint(new NotEqualConstraint(WA, SA));
+    addConstraint(new NotEqualConstraint(NT, SA));
+    addConstraint(new NotEqualConstraint(NT, Q));
+    addConstraint(new NotEqualConstraint(SA, Q));
+    addConstraint(new NotEqualConstraint(SA, NSW));
+    addConstraint(new NotEqualConstraint(SA, V));
+    addConstraint(new NotEqualConstraint(Q, NSW));
+    addConstraint(new NotEqualConstraint(NSW, V));
+    // Añadida restricción
+    addConstraint(new NotEqualConstraint(WA, V));
+  }
 }
--- src/main/java/ssii/p4/MapColoringCSPDemo.java
+++ src/main/java/ssii/p4/MapColoringCSPDemo.java
@@ -9,66 +9,83 @@ import aima.core.search.csp.MinConflictsStrategy;
 import aima.core.search.csp.SolutionStrategy;
 
 /**
- * Demonstrates the performance of different constraint solving strategies.
- * The map coloring problem from the textbook is used as CSP.
- * 
+ * Demonstrates the performance of different constraint solving strategies. The map coloring problem
+ * from the textbook is used as CSP.
+ *
  * @author Ruediger Lunde
  */
 
 public class MapColoringCSPDemo {
-	public static void main(String[] args) {
-		CSP csp = new MapCSP();
-		StepCounter stepCounter = new StepCounter();
-		SolutionStrategy solver;
-		
-		solver = new MinConflictsStrategy(1000);
-		solver.addCSPStateListener(stepCounter);
-		stepCounter.reset();
-		System.out.println("Map Coloring (Minimum Conflicts)");
-		System.out.println(solver.solve(csp.copyDomains()));
-		System.out.println(stepCounter.getResults() + "\n");
-		
-		solver = new ImprovedBacktrackingStrategy(true, true, true, true);
-		solver.addCSPStateListener(stepCounter);
-		stepCounter.reset();
-		System.out.println("Map Coloring (Backtracking + MRV + DEG + AC3 + LCV)");
-		System.out.println(solver.solve(csp.copyDomains()));
-		System.out.println(stepCounter.getResults() + "\n");
-		
-		solver = new BacktrackingStrategy();
-		solver.addCSPStateListener(stepCounter);
-		stepCounter.reset();
-		System.out.println("Map Coloring (Backtracking)");
-		System.out.println(solver.solve(csp.copyDomains()));
-		System.out.println(stepCounter.getResults() + "\n");
-	}
-	
-	/** Counts assignment and domain changes during CSP solving. */
-	protected static class StepCounter implements CSPStateListener {
-		private int assignmentCount = 0;
-		private int domainCount = 0;
-		
-		@Override
-		public void stateChanged(Assignment assignment, CSP csp) {
-			++assignmentCount;
-		}
-		
-		@Override
-		public void stateChanged(CSP csp) {
-			++domainCount;
-		}
-		
-		public void reset() {
-			assignmentCount = 0;
-			domainCount = 0;
-		}
-		
-		public String getResults() {
-			StringBuffer result = new StringBuffer();
-			result.append("assignment changes: " + assignmentCount);
-			if (domainCount != 0)
-				result.append(", domain changes: " + domainCount);
-			return result.toString();
-		}
-	}
+  /**
+   * Entrada para ejecutar las pruebas
+   *
+   * @param args parámetros opcionales de entrada al ser invocado éste método
+   */
+  @SuppressWarnings("nls")
+  public static void main(final String[] args) {
+    final CSP csp = new MapCSP();
+    final StepCounter stepCounter = new StepCounter();
+    SolutionStrategy solver;
+
+    solver = new MinConflictsStrategy(1000);
+    solver.addCSPStateListener(stepCounter);
+    stepCounter.reset();
+    System.out.println("Map Coloring (Minimum Conflicts)");
+    System.out.println(solver.solve(csp.copyDomains()));
+    System.out.println(stepCounter.getResults() + "\n");
+
+    solver = new ImprovedBacktrackingStrategy(true, true, true, true);
+    solver.addCSPStateListener(stepCounter);
+    stepCounter.reset();
+    System.out.println("Map Coloring (Backtracking + MRV + DEG + AC3 + LCV)");
+    System.out.println(solver.solve(csp.copyDomains()));
+    System.out.println(stepCounter.getResults() + "\n");
+
+    solver = new BacktrackingStrategy();
+    solver.addCSPStateListener(stepCounter);
+    stepCounter.reset();
+    System.out.println("Map Coloring (Backtracking)");
+    System.out.println(solver.solve(csp.copyDomains()));
+    System.out.println(stepCounter.getResults() + "\n");
+  }
+
+  /** Counts assignment and domain changes during CSP solving. */
+  protected static class StepCounter implements CSPStateListener {
+    private int assignmentCount = 0;
+    private int domainCount = 0;
+
+    @Override
+    public void stateChanged(final Assignment assignment, final CSP csp) {
+      ++assignmentCount;
+    }
+
+    @Override
+    public void stateChanged(final CSP csp) {
+      ++domainCount;
+    }
+
+    /**
+     * Resetea las asignaciones y el dominio.
+     */
+    public void reset() {
+      assignmentCount = 0;
+      domainCount = 0;
+    }
+
+    /**
+     * Obtiene los cambios realizados en el dominio y en las asignaciones
+     *
+     * @return una cadena de texto que informa de los cambios realizados en las asignaciones y el
+     *         dominio.
+     */
+    @SuppressWarnings("nls")
+    public String getResults() {
+      final StringBuffer result = new StringBuffer();
+      result.append("assignment changes: " + assignmentCount);
+      if (domainCount != 0) {
+        result.append(", domain changes: " + domainCount);
+      }
+      return result.toString();
+    }
+  }
 }
